34168ccf7d55d9599aecc75533d3ac5d4794cc67
boeing7478F <85016918+boeing7478F@users.noreply.github.com>
2021-05-29 16:39:01 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
+    basic.showString("The temperature is " + input.temperature() + " celsius")
+})
+basic.forever(function () {
+    basic.showString("Hello!")
+    basic.showString("The length of hello is " + "Hello".length)
 })
